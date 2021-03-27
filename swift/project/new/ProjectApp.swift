3aa0494906ec514371bf0dd0c42c1d1ff1ee0d29
xinyu <55555321+xinyu889@users.noreply.github.com>
2021-03-27 16:59:14 +0800

    Add files via upload

---
+++ swift/project/new/ProjectApp.swift
@@ -0,0 +1,18 @@
+//
+//  newApp.swift
+//  new
+//
+//  Created by 張馨予 on 2021/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Topic1()
+        }
+            
+    }
+}
